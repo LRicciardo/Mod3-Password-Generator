ed072bd0179159718310b35289ce2a139e5bd872
LRicciardo <liane.ricciardo@gmail.com>
2022-09-16 16:21:22 -0500

    added comments

--- script.js
+++ script.js
@@ -11,28 +11,27 @@ var passwordCriteria = {
 var availableChars = [];
 
 // >>>>>>>>>>>>>>>  program flow
+// >> generateBtn manipulates the HTML button with the id of generate
 // getUserCriteria();
 // buildCharSet();
 // generatePassword();
 // writePassword();
 
-// console.log('var availableChars =>>' + availableChars );
-// console.log('var availableChars typeof =>>' + typeof availableChars);
-// console.log('var availableChars length =>>' + availableChars.length);
-// console.log('var availableChars[1] =>>' + availableChars[1]);
-// generatePassword();
-// console.log('var newPassword =>>' + newPassword);
+// writePassword gets called from the Event Listener then 
+//      calls the other functions.
+
+// future improvements: 
+//    If a user selects a criteria verify that at least one that
+//        criteria is in the password generated.
+//    Improve UI (User Interface) adding a checkbox or list. Something to 
+//        think about.
 
 //  This will query user for Password Criteria
 function getUserCriteria() {
-  console.log('inside getUserCriteria function ==>')
+  // console.log('inside getUserCriteria function ==>')
   var lengthMsg = 'How many characters should the password be? ';
-  lengthMsg += 'Enter a number from 8 to 128. (Default is 8.)';
-  
+      lengthMsg += 'Enter a number from 8 to 128. (Default is 8.)';
   const yesNoMsg = 'Enter "Y" for Yes or "N" for No. (Default is Yes)';
-  const upperMsg = 'Do you want Uppercase characters? ' + yesNoMsg;
-  const numericMsg = 'Do you want Numeric characters? ' + yesNoMsg;
-  const specialMsg = 'Do you want Special characters? ' + yesNoMsg;
   
   // password length user prompt -- If invalid answer, set default is 8
   var promptAnswer = prompt(lengthMsg);
@@ -72,13 +71,13 @@ function getUserCriteria() {
     } else {
       passwordCriteria.specialCharFlag = true;
     }; 
-    console.log('var passwordCriteria =>>');
-    console.table( passwordCriteria );
+    // console.log('var passwordCriteria =>>');
+    // console.table( passwordCriteria );
     return;
   }
   
   function buildCharSet () {
-    console.log('inside buildCharSet function ==>')
+    // console.log('inside buildCharSet function ==>')
     const alphaChars = 'abcdefghijklmnopqrstuvwxyz';
     const numericChars = '0123456789';
     const specialChars = '?!#$%@&';
@@ -96,20 +95,20 @@ function getUserCriteria() {
     };
     // make an array
     availableChars = availableString.split('');
-    console.log('var availableChars =>>' + availableChars);
+    // console.log('var availableChars =>>' + availableChars);
     return;
   };
   
   // build the password from available characters
   function generatePassword() {
-    console.log('inside generatePassword function ==>')
+    // console.log('inside generatePassword function ==>')
     var newPassword = '';
     var i = 0
     while (i < passwordCriteria.pwLength) {
       newPassword += availableChars[Math.floor(Math.random() * availableChars.length)];
       i++;
     };
-    console.log('var newPassword =>>' + newPassword);
+    // console.log('var newPassword =>>' + newPassword);
     return newPassword;
   }
   
@@ -120,7 +119,8 @@ function getUserCriteria() {
     getUserCriteria();
     buildCharSet();
     var password = generatePassword();
-    console.log('var password =>>' + password);
+    // console.log('var password =>>' + password);
+    // text string is passed to the textarea with the password id in the HTML
     var passwordText = document.querySelector("#password");
     
   passwordText.value = password;

6bfda88acf2e9546ab2065e5402b31ee4c307948
LRicciardo <liane.ricciardo@gmail.com>
2022-09-16 09:54:41 -0500

    under development -- UI complete --prettify-ing

--- script.js
+++ script.js
@@ -4,83 +4,128 @@ var generateBtn = document.querySelector("#generate");
 // password criteria variables set with default values
 var passwordCriteria = {
   pwLength: 8,
-  lowerCaseFlag: true,
   upperCaseFlag: true,
   numericFlag: true,
   specialCharFlag: true,
 };
 var availableChars = [];
-var newPassword = [];
 
+// >>>>>>>>>>>>>>>  program flow
+// getUserCriteria();
 // buildCharSet();
-console.log('var availableChars =>>' + availableChars );
-console.log('var availableChars typeof =>>' + typeof availableChars);
-console.log('var availableChars length =>>' + availableChars.length);
-console.log('var availableChars[1] =>>' + availableChars[1]);
 // generatePassword();
-writePassword();
-console.log('var newPassword =>>' + newPassword);
+// writePassword();
 
-function buildCharSet (inputCriteria) {
+// console.log('var availableChars =>>' + availableChars );
+// console.log('var availableChars typeof =>>' + typeof availableChars);
+// console.log('var availableChars length =>>' + availableChars.length);
+// console.log('var availableChars[1] =>>' + availableChars[1]);
+// generatePassword();
+// console.log('var newPassword =>>' + newPassword);
+
+//  This will query user for Password Criteria
+function getUserCriteria() {
+  console.log('inside getUserCriteria function ==>')
+  var lengthMsg = 'How many characters should the password be? ';
+  lengthMsg += 'Enter a number from 8 to 128. (Default is 8.)';
+  
+  const yesNoMsg = 'Enter "Y" for Yes or "N" for No. (Default is Yes)';
+  const upperMsg = 'Do you want Uppercase characters? ' + yesNoMsg;
+  const numericMsg = 'Do you want Numeric characters? ' + yesNoMsg;
+  const specialMsg = 'Do you want Special characters? ' + yesNoMsg;
+  
+  // password length user prompt -- If invalid answer, set default is 8
+  var promptAnswer = prompt(lengthMsg);
+  if (promptAnswer >= 8 &&
+    promptAnswer <= 128) {
+      passwordCriteria.pwLength = promptAnswer;
+    } else {
+      passwordCriteria.pwLength = 8;
+    }; 
+    
+    // Uppercase Message user prompt. If invalid answer, default is true
+    var charMsg = 'Uppercase characters? ';
+    promptAnswer = prompt('Do you want ' + charMsg + yesNoMsg);
+    if (promptAnswer == 'n' ||
+    promptAnswer == 'no') {
+      passwordCriteria.upperCaseFlag = false;
+    } else {
+      passwordCriteria.upperCaseFlag = true;
+    }; 
+    
+    // Numeric Message user prompt. If invalid answer, default is true
+    var charMsg = 'Numeric characters? ';
+    promptAnswer = prompt('Do you want ' + charMsg + yesNoMsg);
+    if (promptAnswer == 'n' ||
+    promptAnswer == 'no') {
+      passwordCriteria.numericFlag = false;
+    } else {
+      passwordCriteria.numericFlag = true;
+    }; 
+    
+    // Scpecial character Message user prompt. If invalid answer, default is true
+    var charMsg = 'Special characters? ';
+    promptAnswer = prompt('Do you want ' + charMsg + yesNoMsg);
+    if (promptAnswer == 'n' ||
+    promptAnswer == 'no') {
+      passwordCriteria.specialCharFlag = false;
+    } else {
+      passwordCriteria.specialCharFlag = true;
+    }; 
+    console.log('var passwordCriteria =>>');
+    console.table( passwordCriteria );
+    return;
+  }
+  
+  function buildCharSet () {
+    console.log('inside buildCharSet function ==>')
     const alphaChars = 'abcdefghijklmnopqrstuvwxyz';
     const numericChars = '0123456789';
     const specialChars = '?!#$%@&';
-    var availableString = '';
-
-    // prompt for password criteria
-let doSwitch = true; 
-  do {
-      passwordCriteria.pwlength = prompt("Please enter the length of the password? (default = 8)")
-    if (passwordCriteria.pwlength != NaN &&
-        passwordCriteria.pwlength >= 8 &&
-        passwordCriteria.pwlength <= 128){
-            doSwitch = false
-        }
-    }
-    while (doSwitch); 
-
-  if (passwordCriteria.lowerCaseFlag) {
-    availableString += alphaChars;
-  };
-  if (passwordCriteria.upperCaseFlag) {
-    availableString += alphaChars.toUpperCase();
-  };
-  if (passwordCriteria.numericFlag) {
-    availableString += numericChars;
-  // availableChars.push(numericChars.split(''));
-  };
-  if (passwordCriteria.specialCharFlag) {
-    availableString += specialChars;
-  // availableChars.push(specialChars.split(''));
-  };
-  availableChars = availableString.split('');
-  return;
-};
-
-// Write password to the #password input
-function writePassword() {
-//   var password = generatePassword();
-  var password = newPassword;
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-console.log(' var passwordCriteria.pwLength ==>' + passwordCriteria.pwLength)
- function generatePassword() {
-   /*  while loop code  to create password*/
-   var i = 0
-   while (i < passwordCriteria.pwLength) {
-     newPassword += availableChars[Math.floor(Math.random() * availableChars.length)];
-     i++;
+    // default character set is lower characters.
+    var availableString = alphaChars;
+    
+    if (passwordCriteria.upperCaseFlag) {
+      availableString += alphaChars.toUpperCase();
     };
-   return 
- }
+    if (passwordCriteria.numericFlag) {
+      availableString += numericChars;
+    };
+    if (passwordCriteria.specialCharFlag) {
+      availableString += specialChars;
+    };
+    // make an array
+    availableChars = availableString.split('');
+    console.log('var availableChars =>>' + availableChars);
+    return;
+  };
+  
+  // build the password from available characters
+  function generatePassword() {
+    console.log('inside generatePassword function ==>')
+    var newPassword = '';
+    var i = 0
+    while (i < passwordCriteria.pwLength) {
+      newPassword += availableChars[Math.floor(Math.random() * availableChars.length)];
+      i++;
+    };
+    console.log('var newPassword =>>' + newPassword);
+    return newPassword;
+  }
+  
+  // Write password to the #password input
+  // This in the main progran flow 
+  function writePassword() {
+    console.log('inside write function ==>')
+    getUserCriteria();
+    buildCharSet();
+    var password = generatePassword();
+    console.log('var password =>>' + password);
+    var passwordText = document.querySelector("#password");
+    
+  passwordText.value = password;
+}
+console.log(">>>> This is before the event listener -- Let's Begin!")
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", buildCharSet);
-
-function randomIdx ()  {
-  return Math.floor(Math.random() * availableChars.length);
-};
+generateBtn.addEventListener("click", writePassword);
